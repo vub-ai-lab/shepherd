d0642e1c71a8fe04a366bfae40030be21a7ab88d
Denis Steckelmacher <steckdenis@yahoo.fr>
2022-01-05 09:55:15 +0100

    Move files from mysite/ to the root directory

--- settings.py
+++ settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database

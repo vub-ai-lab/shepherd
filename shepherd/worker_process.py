6ad63119ebab3d20bc495e6a133733a560ae2473
Denis Steckelmacher <steckdenis@yahoo.fr>
2022-05-18 10:31:39 +0200

    Add author and year in the license

--- shepherd/worker_process.py
+++ shepherd/worker_process.py
@@ -1,5 +1,7 @@
 # This file is part of Shepherd.
 #
+# Copyright 2022 Hélène Plisnier
+#
 # Shepherd is free software: you can redistribute it and/or modify it under the
 # terms of the GNU Affero General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
@@ -7,9 +9,9 @@
 #
 # Shepherd is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
+# You should have received a copy of the GNU Affero General Public License along
 # with Shepherd. If not, see <https://www.gnu.org/licenses/>.
 
 import multiprocessing
